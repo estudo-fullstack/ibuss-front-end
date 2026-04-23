ac5745cca8fa50358fbffd3833da4269a700136e
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-04-23 10:00:08 -0300

    feat(register): configure QueryClient for API queries

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 
 import App from "./App";
+import { queryClient } from "./services/queryClient";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
