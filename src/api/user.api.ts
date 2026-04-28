e6714ddf3459960e3e935f18f3f2f6df491d08dc
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-04-28 09:38:53 -0300

    fix: update POST API endpoint to align with backend route

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -2,7 +2,7 @@ import api from "../services/axios.ts";
 import type { CreateUserType, LoginUserType } from "./types.ts";
 
 export async function createUser(data: CreateUserType) {
-  const response = await api.post("/api/auth/register", data);
+  const response = await api.post("/users/register", data);
   return response.data;
 }
 
